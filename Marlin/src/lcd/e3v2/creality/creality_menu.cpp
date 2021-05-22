9941a5811e29791ba529d7587d381b52420cae2d
Alexey Galakhov <agalakhov@gmail.com>
2021-05-22 21:43:23 +0200

    Move menus to separate file

---
+++ Marlin/src/lcd/e3v2/creality/creality_menu.cpp
@@ -0,0 +1,74 @@
+/**
+ * Marlin 3D Printer Firmware
+ * Copyright (c) 2020 MarlinFirmware [https://github.com/MarlinFirmware/Marlin]
+ *
+ * Based on Sprinter and grbl.
+ * Copyright (c) 2011 Camiel Gubbels / Erik van der Zalm
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
+/**
+ * DWIN by Creality3D
+ * Rewrite and Extui Port by Jacob Myers
+ * Refactor by Alexey Galakhov
+ */
+
+#include "creality_menu.h"
+
+using namespace Creality;
+using DWIN::Icon;
+
+const Menu Creality_mainMenu = {
+  "Main Menu",
+  MenuType_Icons {
+      Geometry::mainMenuItemSize,
+      Geometry::mainMenuGrid,
+      2
+  },
+  {
+    { { Icon::Print_0, Icon::Print_1 }, "Print", Action_Dummy{} },
+    { { Icon::Prepare_0, Icon::Prepare_1 }, "Prepare", Action_Dummy{} },
+    { { Icon::Control_0, Icon::Control_1 }, "Control", Action_Dummy{} },
+    #if ANY(HAS_ONESTEP_LEVELING, AUTO_BED_LEVELING_UBL, PROBE_MANUALLY)
+      { { Icon::Leveling_0, Icon::Leveling_1 }, "Level", Action_Dummy{} },
+    #else
+      { { Icon::Info_0, Icon::Info_1}, "Info", Action_Dummy{} },
+    #endif
+    Creality::EndMenu
+  }
+};
+
+extern bool printing;
+static bool Helper_IsPrinting() {
+  return printing;
+}
+
+const Menu Creality_printMenu = {
+  nullptr,
+  Creality::MenuType_Icons {
+      Geometry::printMenuItemSize,
+      Geometry::printMenuGrid,
+      3
+  },
+  {
+    { { Icon::Setup_0, Icon::Setup_1 }, "Tune", Action_Dummy{} },
+    { { Icon::Pause_0, Icon::Pause_1 }, "Pause", Action_Dummy{}, Only_If(Helper_IsPrinting) },
+    { { Icon::Continue_0,  Icon::Continue_1}, "Print", Action_Dummy{}, Only_If_Not(Helper_IsPrinting) },
+    { { Icon::Stop_0, Icon::Stop_1}, "Stop", Action_Dummy{} },
+    Creality::EndMenu
+  }
+};
+
