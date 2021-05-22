e13e9d38600b5850f3bb64f346f4a9a744621b16
Alexey Galakhov <agalakhov@gmail.com>
2021-05-23 00:38:52 +0200

    List menu draft

--- Marlin/src/lcd/e3v2/creality/menu.cpp
+++ Marlin/src/lcd/e3v2/creality/menu.cpp
@@ -29,6 +29,8 @@
 #include "menu.h"
 #include "dwin.h"
 
+using std::holds_alternative;
+
 namespace Creality {
 
   void MenuEngine::Draw_IconicMenu(const MenuType_Icons& type, const MenuItem items[], uint16_t selection) {
@@ -51,6 +53,21 @@ namespace Creality {
     }
   }
 
+  void MenuEngine::Draw_ListMenu(const MenuType_List& type, const MenuItem items[], uint16_t selection) {
+    uint8_t idx = 0;
+    Point pos = {
+      Geometry::listItemCursorWidth + Geometry::listItemLeftMargin + Geometry::listItemLeftPadding,
+      Geometry::titleHeight + Geometry::listItemPadding
+    };
+    for (const MenuItem * item = items; idx < 4 && item->text != nullptr; ++item) {
+      if (item->predicate()) {
+        ++idx;
+        this->Draw_ListItem(type, *item, pos, (idx == selection));
+        pos.x += Geometry::listItemIconSize.h + 2 * Geometry::listItemPadding + 1;
+      }
+    }
+  }
+
   void MenuEngine::Draw_IconicItem(const MenuType_Icons& type, const MenuItem& item, Point pos, bool selected) {
     constexpr auto font = Geometry::menuFont;
     const auto highlightColor = Color_White; // TODO GetColor(eeprom_settings.highlight_box, Color_White);
@@ -63,6 +80,26 @@ namespace Creality {
     const Rectangle textRect = rect.Bottom(Geometry::iconMenuTextHeight);
     const auto textPos = Geometry::Text_Inscribe(textRect, font, item.text);
     DWIN_Draw_String(false, false, font, Color_White, Color_Bg_Blue, textPos.x, textPos.y, item.text);
+  }
+
+  void MenuEngine::Draw_ListItem(const MenuType_List& type, const MenuItem& item, Point pos, bool selected) {
+    constexpr auto font = Geometry::menuFont;
+    const auto lineColor = Line_Color; // TODO GetColor(eeprom_settings.menu_split_line, Line_Color, true);
+    const auto icon = (selected && item.icons.selected != DWIN::NoIcon) ? item.icons.selected : item.icons.normal;
+    const auto iconPos = pos;
+    const Point textPos = {
+      static_cast<uint16_t>(pos.x + Geometry::listItemIconSize.w + Geometry::listItemTextOffset),
+      static_cast<uint16_t>(pos.y + (Geometry::listItemIconSize.h - Geometry::Font_CharSize(font).h) / 2)
+    };
+    DWIN_Draw_String(false, false, font, Color_White, Color_Bg_Black, textPos.x, textPos.y, item.text);
+    if (icon != DWIN::NoIcon) {
+      DWIN_ICON_Show(DWIN::ICON, icon, iconPos.x, iconPos.y);
+    }
+    if (holds_alternative<Action_EnterMenu>(item.action)) {
+      DWIN_ICON_Show(DWIN::ICON, DWIN::Icon::More, iconPos.x + Geometry::listItemRightIconOffset, iconPos.y);
+    }
+    const uint16_t lineY = pos.y + Geometry::listItemIconSize.h + Geometry::listItemPadding;
+    DWIN_Draw_Line(lineColor, Geometry::listItemLineLeft, lineY, Geometry::listItemLineRight, lineY);
   }
 
 } // namespace Creality
