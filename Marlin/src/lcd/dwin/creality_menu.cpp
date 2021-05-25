7a973eeb3dee71e6eb4c7fcd3d35f22586b7ca2d
Alexey Galakhov <agalakhov@gmail.com>
2021-05-25 23:43:03 +0200

    Populating menus

--- Marlin/src/lcd/dwin/creality_menu.cpp
+++ Marlin/src/lcd/dwin/creality_menu.cpp
@@ -27,60 +27,34 @@
  */
 
 #include "creality_menu.h"
+#include "creality_actions.h"
 #include "creality_dwin.h"
+#include "../../inc/MarlinConfigPre.h"
 
 using namespace Creality;
 using DWIN::Icon;
 
 // Menu structures MUST land in .rodata section. Make sure they all are const constexpr.
 
-const constexpr Creality::Menu Creality_mainMenu = {
-  "Main Menu",
-  MenuType_Icons {
-      Geometry::mainMenuItemSize,
-      Geometry::mainMenuGrid,
-      2
-  },
-  {
-    { { Icon::Print_0, Icon::Print_1 }, "Print", Action_Do{ CrealityDWINClass::Tmp_PrintClicked } },
-    { { Icon::Prepare_0, Icon::Prepare_1 }, "Prepare", Action_Do{ CrealityDWINClass::Tmp_PrepareClicked } },
-    { { Icon::Control_0, Icon::Control_1 }, "Control", Action_Do{ CrealityDWINClass::Tmp_SettingsClicked } },
-    #if ANY(HAS_ONESTEP_LEVELING, AUTO_BED_LEVELING_UBL, PROBE_MANUALLY)
-      { { Icon::Leveling_0, Icon::Leveling_1 }, "Level", Action_Do{ CrealityDWINClass::Tmp_LevelClicked } },
-    #else
-      { { Icon::Info_0, Icon::Info_1}, "Info", Action_Do{ CrealityDWINClass::Tmp_InfoClicked } },
-    #endif
-    Creality::EndMenu()
-  }
-};
-
-extern bool printing;
-static bool Helper_IsPrinting() {
-  return printing;
-}
-
-const constexpr Creality::Menu Creality_printMenu = {
-  nullptr,
-  Creality::MenuType_Icons {
-      Geometry::printMenuItemSize,
-      Geometry::printMenuGrid,
-      3
-  },
-  {
-    { { Icon::Setup_0, Icon::Setup_1 }, "Tune", Action_Dummy{} },
-    { { Icon::Pause_0, Icon::Pause_1 }, "Pause", Action_Dummy{}, Only_If(Helper_IsPrinting) },
-    { { Icon::Continue_0,  Icon::Continue_1}, "Print", Action_Dummy{}, Only_If_Not(Helper_IsPrinting) },
-    { { Icon::Stop_0, Icon::Stop_1}, "Stop", Action_Dummy{} },
-    Creality::EndMenu()
-  }
-};
 
 static const constexpr Creality::Menu moveMenu = {
   "Move",
-  MenuType_List{},
+  MenuType_List{
+    .on_enter = nullptr,
+    .on_leave = CrealityActions::RetractProbe,
+  },
   {
-    { Icon::Back,         "Back",             Action_LeaveMenu{} },
-    // TODO
+    { Icon::Back,   "Back",     Action_LeaveMenu{} },
+//TODO    { Icon::MoveX,  "Move X",   Action_Value{current_position.x, X_MIN_POS, X_MAX_POS, 10} },
+//TODO    { Icon::MoveY,  "Move Y",   Action_Value{current_position.y, Y_MIN_POS, Y_MAX_POS, 10} },
+//TODO    { Icon::MoveZ,  "Move Z",   Action_Value{current_position.z, Z_MIN_POS, Z_MAX_POS, 10} },
+    #if HAS_HOTEND
+// TODO      { Icon::Extruder, "Extruder", Action_Value
+    #endif
+    #if HAS_BED_PROBE
+//TODO      { Icon::StockConfiguration, "Probe", Action_Checkbox{probe_deployed},
+    #endif
+//TODO    { Icon::Axis,   "Live Movement",    Action_Checkbox{livemove} },
     EndMenu()
   }
 };
@@ -89,8 +63,12 @@ static const constexpr Creality::Menu homeMenu = {
   "Homing Menu",
   MenuType_List{},
   {
-    { Icon::Back,         "Back",             Action_LeaveMenu{} },
-    // TODO
+    { Icon::Back,       "Back",               Action_LeaveMenu{} },
+    { Icon::Homing,     "Home All",           Action_Do{CrealityActions::HomeAll} },
+//TODO    { Icon::MoveX,      "Home X",             Action_GCode_Sync{Home, "G28 X"} },
+//TODO    { Icon::MoveY,      "Home Y",             Action_GCode_Sync{Home, "G28 Y"} },
+//TODO    { Icon::MoveZ,      "Home Z",             Action_GCode_Sync{Home, "G28 Z"} },
+//TODO    { Icon::SetHome,    "Set Home Position",  Action_GCode_Beep{"G92 X0 Y0 Z0"} },
     EndMenu()
   }
 };
@@ -105,46 +83,36 @@ static const constexpr Creality::Menu preheatMenu = {
   }
 };
 
+static const constexpr Creality::Menu manualLevelingMenu = {
+  "Manual Leveling",
+  MenuType_List {
+    .on_enter = CrealityActions::HomeAndDisableLeveling,
+    .on_leave = CrealityActions::ReEnableLeveling,
+  },
+  {
+    { Icon::Back,       "Back",             Action_LeaveMenu{}                      },
+    // TODO
+    EndMenu()
+  }
+};
+
 static const constexpr Creality::Menu prepareMenu = {
   "Prepare",
   MenuType_List{},
   {
-    { Icon::Back,         "Back",             Action_LeaveMenu{} },
-    { Icon::Axis,         "Move",             Action_EnterMenu{moveMenu} },
-//  { Icon::CloseMotor, "Disable Stepper",  Action_GCode{"M84"} },
-    { Icon::SetHome,      "Homing",           Action_EnterMenu{homeMenu} },
-
-//  { Icon::PrintSize,  "Manual Leveling",  Action_Do{ manual leveling },
-/*         if (axes_should_home()) {
-              Popup_Handler(Home);
-              gcode.home_all_axes(true);
-            }
-            #if HAS_LEVELING
-              level_state = planner.leveling_active;
-              set_bed_leveling_enabled(false);
-            #endif
-            Draw_Menu(ManualLevel);
-          }
-*/
+    { Icon::Back,       "Back",             Action_LeaveMenu{}                      },
+    { Icon::Axis,       "Move",             Action_EnterMenu{moveMenu}              },
+//TODO    { Icon::CloseMotor,   "Disable Stepper",  Action_GCode{"M84"} },
+    { Icon::SetHome,    "Homing",           Action_EnterMenu{homeMenu}              },
+    { Icon::PrintSize,  "Manual Leveling",  Action_EnterMenu{manualLevelingMenu}    },
 
     #if HAS_ZOFFSET_ITEM
-//  { Icon::Zoffset,      "Z-Offset",         Action_Do{ z_offset } },
-/*
-              #if HAS_LEVELING
-                level_state = planner.leveling_active;
-                set_bed_leveling_enabled(false);
-              #endif
-              Draw_Menu(ZOffset);
-*/
+    { Icon::Zoffset,      "Z-Offset",         Action_EnterMenu{zOffsetMenu}         },
     #endif
 
     #if HAS_PREHEAT
-      { Icon::Temperature,    "Preheat",      Action_EnterMenu{preheatMenu} },
-//    { Icon::Cool,           "Cooldown",     Action_Do{ cooldown }, 
-/*
-  thermalManager.zero_fan_speeds();
-  thermalManager.disable_all_heaters();
- */
+      { Icon::Temperature,    "Preheat",      Action_EnterMenu{preheatMenu}         },
+      { Icon::Cool,           "Cooldown",     Action_Do{CrealityActions::Cooldown}  },
     #endif
 
     #if ENABLED(ADVANCED_PAUSE_FEATURE)
@@ -171,6 +139,47 @@ static const constexpr Creality::Menu prepareMenu = {
       },
     #endif
 
+    EndMenu()
+  }
+};
+
+const constexpr Creality::Menu Creality_mainMenu = {
+  "Main Menu",
+  MenuType_Icons {
+      Geometry::mainMenuItemSize,
+      Geometry::mainMenuGrid,
+      2
+  },
+  {
+    { { Icon::Print_0, Icon::Print_1 },         "Print",    Action_Do{ CrealityDWINClass::Tmp_PrintClicked }    },
+    { { Icon::Prepare_0, Icon::Prepare_1 },     "Prepare",  Action_Do{ CrealityDWINClass::Tmp_PrepareClicked }  },
+    { { Icon::Control_0, Icon::Control_1 },     "Control",  Action_Do{ CrealityDWINClass::Tmp_SettingsClicked } },
+    #if ANY(HAS_ONESTEP_LEVELING, AUTO_BED_LEVELING_UBL, PROBE_MANUALLY)
+      { { Icon::Leveling_0, Icon::Leveling_1 }, "Level",    Action_Do{ CrealityDWINClass::Tmp_LevelClicked }    },
+    #else
+      { { Icon::Info_0, Icon::Info_1},          "Info",     Action_Do{ CrealityDWINClass::Tmp_InfoClicked }     },
+    #endif
+    EndMenu()
+  }
+};
+
+extern bool printing;
+static bool Helper_IsPrinting() {
+  return printing;
+}
+
+const constexpr Creality::Menu Creality_printMenu = {
+  nullptr,
+  Creality::MenuType_Icons {
+      Geometry::printMenuItemSize,
+      Geometry::printMenuGrid,
+      3
+  },
+  {
+    { { Icon::Setup_0, Icon::Setup_1 },         "Tune",     Action_Dummy{} },
+    { { Icon::Pause_0, Icon::Pause_1 },         "Pause",    Action_Dummy{}, Only_If(Helper_IsPrinting) },
+    { { Icon::Continue_0,  Icon::Continue_1},   "Print",    Action_Dummy{}, Only_If_Not(Helper_IsPrinting) },
+    { { Icon::Stop_0, Icon::Stop_1},            "Stop",     Action_Dummy{} },
     EndMenu()
   }
 };
