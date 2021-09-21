7e83817e4379726b29dfc75ca23d02b8460716da
Alexey Galakhov <agalakhov@gmail.com>
2021-09-22 00:35:10 +0200

    Use new generic iconic menu for main and print menus

--- Marlin/src/lcd/e3v2/creality/dwin_defs.h
+++ Marlin/src/lcd/e3v2/creality/dwin_defs.h
@@ -181,3 +181,42 @@ namespace DWIN {
   };
 
 } // namespace DWIN
+
+
+// TODO refactor this
+
+#define Custom_Colors       10
+#define Color_White         0xFFFF
+#define Color_Light_White   0xBDD7
+#define Color_Green         0x07E0
+#define Color_Light_Green   0x3460
+#define Color_Cyan          0x07FF
+#define Color_Light_Cyan    0x04F3
+#define Color_Blue          0x015F
+#define Color_Light_Blue    0x3A6A
+#define Color_Magenta       0xF81F
+#define Color_Light_Magenta 0x9813
+#define Color_Red           0xF800
+#define Color_Light_Red     0x8800
+#define Color_Orange        0xFA20
+#define Color_Light_Orange  0xFBC0
+#define Color_Yellow        0xFF0F
+#define Color_Light_Yellow  0x8BE0
+#define Color_Brown         0xCC27
+#define Color_Light_Brown   0x6204
+#define Color_Black         0x0000
+#define Color_Grey          0x18E3
+#define Color_Bg_Window     0x31E8  // Popup background color
+#define Color_Bg_Blue       0x1125  // Dark blue background color
+#define Color_Bg_Black      0x0841  // Black background color
+#define Color_Bg_Red        0xF00F  // Red background color
+#define Popup_Text_Color    0xD6BA  // Popup font background color
+#define Line_Color          0x3A6A  // Split line color
+#define Rectangle_Color     0xEE2F  // Blue square cursor color
+#define Percent_Color       0xFE29  // Percentage color
+#define BarFill_Color       0x10E4  // Fill color of progress bar
+#define Select_Color        0x33BB  // Selected color
+#define Check_Color         0x4E5C  // Check-box check color
+#define Confirm_Color       0x34B9
+#define Cancel_Color        0x3186
+
